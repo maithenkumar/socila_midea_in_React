ed3265d9726390ff3bf0885f89ac43154487fe35
maithenkumar <maitheen1905@gmail.com>
2023-07-19 23:50:11 +0530

    nothind

---
+++ src/pages/Auth/Auth.jsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import { BrowserRouter as   Route  } from 'react-router-dom';
+
+import "./Auth.css";
+import Logo from "../../img/logo.png";
+import Home from "../home/Home";
+const home = () => <Home/>;
+const Auth = () => {
+  return (
+    <div className="Auth">
+      <div className="a-left">
+        <img src={Logo} alt="" />
+        <div className="Webname">
+          <h1>Maithenkumar Sample Website</h1>
+          <h6>Dont press that Sigin Button Please scroll Down because its only staic Website  </h6>
+        </div>
+      </div>
+
+      <LogIn/>
+    </div>
+  );
+};
+function LogIn() {
+    return (
+      <div className="a-right">
+        <form className="infoForm authForm">
+          <h3>Log In</h3>
+  
+          <div>
+            <input
+              type="text"
+              placeholder="Username"
+              className="infoInput"
+              name="username"
+            />
+          </div>
+  
+          <div>
+            <input
+              type="password"
+              className="infoInput"
+              placeholder="Password"
+              name="password"
+            />
+          </div>
+  
+          <div>
+              <span style={{ fontSize: "12px" }}>
+                Don't have an account <button onClick={SignUp} >Sign up</button>
+              </span>
+             
+            <button className="button infoButton" > <Route exact path="/" component={home} /> Sigin</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+function SignUp() {
+  return (
+    <div className="a-right">
+      <form className="infoForm authForm">
+        <h3>Sign up</h3>
+
+        <div>
+          <input
+            type="text"
+            placeholder="First Name"
+            className="infoInput"
+            name="firstname"
+          />
+          <input
+            type="text"
+            placeholder="Last Name"
+            className="infoInput"
+            name="lastname"
+          />
+        </div>
+
+        <div>
+          <input
+            type="text"
+            className="infoInput"
+            name="username"
+            placeholder="Usernames"
+          />
+        </div>
+
+        <div>
+          <input
+            type="text"
+            className="infoInput"
+            name="password"
+            placeholder="Password"
+          />
+          <input
+            type="text"
+            className="infoInput"
+            name="confirmpass"
+            placeholder="Confirm Password"
+          />
+        </div>
+
+        <div>
+            <span style={{fontSize: '12px'}}>Already have an account. Login!</span>
+        </div>
+        <button className="button infoButton" type="submit">Signup</button>
+      </form>
+    </div>
+  );
+}
+
+export default Auth;
